18ac85c3c696fdf69a584af9462713fe8a123635
AbhigyaWangoo <abhigyawangoo@gmail.com>
2024-01-27 19:00:16 -0800

    Shifted to vector struct

--- src/bin/cache/mod.rs
+++ src/bin/cache/mod.rs
@@ -1,6 +1,2 @@
 pub mod node;
 pub mod btree;
-
-pub fn cache_hello_world() {
-    println!("called `cache::cache_hello_world()`");
-}
--- src/bin/cache/node.rs
+++ src/bin/cache/node.rs
@@ -1,15 +1,21 @@
+use std::{ptr, vec};
 
-pub struct Node {
+
+pub struct Vidvec {
     id: String,
-    creation_time: u32,
     vector_size: u32,
     vector: Vec<u32>,
-    payload_str: String,
+    payload_str: String,   
 }
 
-impl Node {
-    // Constructor method to create a new Node with a specified vector size
-    pub fn new(id: String, creation_time: u32, vector_size: u32, vector: Vec<u32>, payload_str: String) -> Result<Self, String> {
+pub struct Node {
+    left: *mut Node,
+    right: *mut Node,
+    vid_vector: Vidvec,
+}
+
+impl Vidvec {
+    pub fn new(id: String, vector_size: u32, vector: Vec<u32>, payload_str: String) -> Result<Self, String> {
         // Initialize the vector with zeros and a specified length
         if vector.len() != vector_size.try_into().unwrap() {
             // Throw this error here: The vector fed in was not of the provided vector size
@@ -17,20 +23,13 @@ impl Node {
         }
 
         // Create and return a new Node instance
-        Ok(Node {
-            id,
-            creation_time,
-            vector_size,
-            vector,
-            payload_str,
-        })
+        Ok(Vidvec { id: id, vector_size: vector_size, vector: vector, payload_str: payload_str })
     }
 
     // Getter methods for each field
     pub fn get_id(&self) -> &String {
         &self.id
     }
-
 }
 
 #[cfg(test)]
@@ -40,16 +39,24 @@ mod tests {
     #[test]
     fn test_creation_simple() {
         let node_id = "node_id";
-        let node: Result<Node, String> = Node::new(node_id.to_string(), 123, 5, vec![0,1,2,3,4], "payload".to_string());
-        match node {
-            Ok(node_instance) => {
+        let vect: Result<Vidvec, String> = Vidvec::new(node_id.to_string(), 5, vec![0,1,2,3,4], "payload".to_string());
+        match vect {
+            Ok(vect_instance) => {
                 // Accessing and printing the id field
-                assert!(node_instance.get_id().as_str() == node_id.to_string())
+                assert!(vect_instance.get_id().as_str() == node_id.to_string())
             }
             Err(error) => {
                 // Handling the error
                 println!("Error creating node: {}", error);
             }
         }
+    }
+
+    #[test]
+    fn test_size_mismatch() {
+        let size = 5;
+        let node_id: &str = "node_id";
+        let vect: Result<Vidvec, String> = Vidvec::new(node_id.to_string(), size, vec![0,1,2,3], "payload".to_string());
+        assert!(vect.is_err());
     }
 }
